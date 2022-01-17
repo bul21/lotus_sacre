25fdc802a755c31fda8b7f375a93875231b2da84
Helene Petrilli <deco02200@gmail.com>
2022-01-17 14:53:12 +0100

    page de contact avec formulaire

---
+++ src/Controller/ContactController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\ContactType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ContactController extends AbstractController
+{
+    /**
+     * @Route("/contact", name="contact")
+     */
+    public function index(Request $request): Response
+    {
+
+
+        $form= $this->createForm(ContactType::class);
+        $form->handleRequest($request);
+        $data=$form->getData();
+        
+
+        if($form->isSubmitted() && $form->isValid()){
+            $email=$data["email"];
+            $nom=$data["nom"];
+
+            return $this->render('contact/success.html.twig',[
+                'email'=>$email,
+                'nom'=>$nom
+            ]);
+
+        }else{
+            return $this->renderForm('contact/index.html.twig',[
+                'controller_name' => 'ContactController',
+                'formulaire'=>$form 
+            ]);
+        } 
+
+        
+       
+    }
+}
