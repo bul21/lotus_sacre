1c0d93d13df510ae28e34a219927b2a9151cc053
Helene Petrilli <deco02200@gmail.com>
2022-02-24 13:16:28 +0100

    finition front end

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -20,32 +20,37 @@ class ContactController extends AbstractController
 
 /* c'est un formulaire de contact à remplir pour l'utilisateur  */
         $form= $this->createForm(ContactType::class);
-        return $this->renderForm('contact/index.html.twig',[
-            'controller_name' =>'ContactController',
-            'formulaire' =>$form
-
-        ]);
         $form->handleRequest($request);
-        $data=$form->getData();
-        
-        
-/* c'est un formulaire  avec un bouton "envoyer" avec une condition: si le formulaire est rempli correctement 
- et envoyé, c'est écrit succes
-sinon le formulaire reste vide*/
+
         if($form->isSubmitted() && $form->isValid()){
+            $data=$form->getData();
             $email=$data["email"];
             $nom=$data["nom"];
 
             return $this->render('contact/success.html.twig',[
                 'email'=>$email,
                 'nom'=>$nom
-            ]);
 
+            ]);
         }else{
             return $this->renderForm('contact/index.html.twig',[
                 'controller_name' => 'ContactController',
                 'formulaire'=>$form 
             ]);
+
+
+
+       
+        
+        
+        
+        
+/* c'est un formulaire  avec un bouton "envoyer" avec une condition: si le formulaire est rempli correctement 
+ et envoyé, c'est écrit succes
+sinon le formulaire reste vide*/
+       
+
+       
         } 
 
         
